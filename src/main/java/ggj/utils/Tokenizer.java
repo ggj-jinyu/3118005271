e02053acf397278a39805eda4e8dea38b6063f24
jinyu <70934604+ggj-jinyu@users.noreply.github.com>
2020-09-24 21:29:42 +0800

    Update Tokenizer.java

--- src/main/java/ggj/utils/Tokenizer.java
+++ src/main/java/ggj/utils/Tokenizer.java
@@ -4,7 +4,6 @@ import com.hankcs.hanlp.HanLP;
 import com.hankcs.hanlp.seg.common.Term;
 import ggj.bean.Word;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,12 +24,5 @@ public class Tokenizer {
         //2、重新封装到Word对象中（term.word代表分词后的词语，term.nature代表改词的词性）
         return termList.stream().map(term ->
                 new Word(term.word, term.nature.toString())).collect(Collectors.toList());
-    }
-
-    public static String formatPrint(double ans){
-        double ansPer = ans * 100.0;
-        String exactAnsPer = String.valueOf(ansPer);
-        BigDecimal decimal = new BigDecimal(exactAnsPer);
-        return decimal.setScale(2,BigDecimal.ROUND_HALF_UP).toPlainString();
     }
 }
